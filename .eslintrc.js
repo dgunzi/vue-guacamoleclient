68ea8eb7bb35362b60c5962d2b30a89ef66c0407
dgunzi <dyfjys@163.com>
2019-12-09 17:08:14 +0800

    增加eslint忽略

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    env: {
+        browser: true,
+    },
+    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+    extends: 'standard',
+    // required to lint *.vue files
+    plugins: [
+        'html'
+    ],
+    // add your custom rules here
+    'rules': {
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        // allow async-await
+        'generator-star-spacing': 0,
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        // 不需要
+        'space-before-function-paren': 0,
+        'eol-last': 0,
+        'no-extra-semi': 0,
+        'semi': 0,
+        'indent': 0,
+        'no-useless-escape': 0,
+        'new-cap': 0,
+        'no-new-func': 0
+    }
+}
